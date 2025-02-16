eb947d99afb795e6118db814697fc1abbd421d25
Andrey Kirik <dev.andrey.kirik@gmail.com>
2025-02-16 23:19:21 +0500

    feat: add snippet 1

---
+++ M1 - Intro to serverless/1 - Start with data/snippet1/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+func handler(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	message, err := json.Marshal("Hello from Lambda!")
+	if err != nil {
+		return events.APIGatewayProxyResponse{StatusCode: 500}, err
+	}
+
+	return events.APIGatewayProxyResponse{
+		StatusCode: 200,
+		Body:       string(message),
+		Headers:    map[string]string{"Content-Type": "application/json"},
+	}, nil
+}
+
+func main() {
+	lambda.Start(handler)
+}
